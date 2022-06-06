22a59b73b01402b8a4691c9f631e8807a2862771
lulu-ana <84982184+lulu-ana@users.noreply.github.com>
2022-06-06 13:34:36 -0400

    Update CodeDemo.cpp

--- Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
+++ Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
@@ -2,9 +2,4 @@
 // Exercise 01_03
 // Hello World, by Eduardo Corpeño 
 
-#include <iostream>
 
-int main(){
-    std::cout << "Hi there!" << sted::end1;
-    return(0); #0 means no errors came up in the program
-} 
